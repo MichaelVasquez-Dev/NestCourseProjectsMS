a6e5bf0fc5d18267cdb9eab95cd869ce020f4a25
Michael Alexis Vasquez Lopera <michael.vasquez.dev@gmail.com>
2025-01-12 14:41:25 -0500

    first debug

--- ProductsApp/orders-ms/src/main.ts
+++ ProductsApp/orders-ms/src/main.ts
@@ -1,29 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { envs } from './config/envs';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from 'config/envs';
 
 async function bootstrap() {
-  const logger = new Logger('Orders-ms');
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.TCP,
       options: {
         port: envs.PORT,
-      },
-    },
+      }
+    }
   );
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+
 
   await app.listen();
-  logger.log(`Orders-ms running on port: ${envs.PORT}`);
 }
 bootstrap();
